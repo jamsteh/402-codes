e9782709de0227b0e6621c2c82f22bedb11e7f79
masongreenblatt <mason.greenblatt@tamu.edu>
2022-01-31 14:06:25 -0600

    updated thermal file (not done yet)

---
+++ design-parameters/thermal/orbits.py
@@ -0,0 +1,14 @@
+from numpy import radians
+from scipy.constants import kilo
+
+from orbital import earth, KeplerianElements, Maneuver, plot, plot3d
+# Create molniya orbit from period and eccentricity
+from orbital import earth_sidereal_day
+molniya = KeplerianElements.with_period(
+    earth_sidereal_day / 2, e=0.741, i=radians(63.4), arg_pe=radians(270),
+    body=earth)
+
+# Simple circular orbit
+orbit = KeplerianElements.with_altitude(1000 * kilo, body=earth)
+
+plot(molniya)
